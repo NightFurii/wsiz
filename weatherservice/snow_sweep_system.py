1382a2e1c6ce9a3ba62606760c5776eb34aed7d3
NightFurii <98234065+NightFurii@users.noreply.github.com>
2023-12-07 13:17:27 +0100

    Update snow_sweep_system.py

--- weatherservice/snow_sweep_system.py
+++ weatherservice/snow_sweep_system.py
@@ -1,6 +1,8 @@
+import asyncio
 from asyncio import run, sleep
 from loguru import logger
-
+import random
+from streets import create_city
 from streets import Street, SnowSweeper
 
 
@@ -16,8 +18,10 @@ class WeatherService:
     async def initialize(self):
         logger.info('Service weather initializing')
         while True:
-            await sleep(1)
-            # update self.current_snow_fall_rate
+            await sleep(2)  # Zmiana na 2 sekundy
+            new_rate = random.uniform(0, 2)
+            logger.info(f"Updated snow fall rate: {new_rate}")
+            self.current_snow_fall_rate = new_rate
 
     async def get_current_snow_fall_rate(self):
         return self.current_snow_fall_rate
@@ -29,6 +33,8 @@ class StreetMonitoringService:
     """
 
     def __init__(self, city: dict[str, Street], weather: WeatherService):
+        self.city = city
+        self.weather = weather
         self.current_snow_fall_rate = 0
 
     async def initialize(self):
@@ -38,9 +44,20 @@ class StreetMonitoringService:
             await self.update_street_snow_cover()
 
     async def update_street_snow_cover(self):
-        # use self.weather + some small random variations
-        # update snow cover for each of the streets of self.city
-        pass
+        for street_name, street in self.city.items():
+            # X to aktualny current_snow_fall_rate z serwisu pogodowego
+            X = await self.weather.get_current_snow_fall_rate()
+
+            # Y to mała liczba losowa z przedziału [0, 1]
+            Y = random.uniform(0, 1)
+
+            # Aktualizacja snow cover dla ulicy
+            street.snow_cover_cm += (X + Y)
+
+            logger.info(f"Updated snow cover for {street_name}: {street.snow_cover_cm}")
+
+        # Aktualizacja self.current_snow_fall_rate
+        self.current_snow_fall_rate = X
 
 
 class SnowSweeperDispatcher:
@@ -56,29 +73,57 @@ class SnowSweeperDispatcher:
             await self.check_city_dispatch_sweeper()
 
     async def check_city_dispatch_sweeper(self):
-        # check if some streets are _really_ under snow
-        # if so -- check if some dispatcher is in length=None
-        # launch self.dispatch_to_sweep_street()
-        pass
+        for street_name, street in self.city.items():
+            logger.info(f'Checking {street_name} with snow cover: {street.snow_cover_cm}')
+            if street.snow_cover_cm > 4:  # Zmienić na odpowiednią wartość progową
+               logger.info(f'Snow cover for {street_name} is above the threshold.')
+               available_sweeper = self.find_available_sweeper()
+               if available_sweeper:
+                   logger.info(f'Dispatching sweeper for {street_name}.')
+                   await self.dispatch_to_sweep_street(street, available_sweeper)
+            else:
+                logger.info('No available sweeper.')
 
-    async def dispatch_to_sweep_street(self, street: Street, sweeper: SnowSweeper):
 
-        cover = street.snow_cover_cm
-        street.snow_cover_cm = 0
-        logger.info(f'sweeper {sweeper.id} sweeping {street.name}')
-        sweeper.location = street.name
 
-        await sleep(street.length * cover)
+async def dispatch_to_sweep_street(self, street: Street, sweeper: SnowSweeper):
+    cover = street.snow_cover_cm
+    street.snow_cover_cm = 0
+    logger.info(f'sweeper {sweeper.id} sweeping {street.name}')
+    sweeper.location = street.name
 
-        logger.info(f'sweeper {sweeper.id} sweeping {street.name} complete')
-        sweeper.location = None
+    await sleep(street.length * cover)
 
+    logger.info(f'sweeper {sweeper.id} sweeping {street.name} complete')
+    sweeper.location = None
+
+    def find_available_sweeper(self):
+        for sweeper in self.sweepers:
+            if sweeper.location is None:
+                return sweeper
+        return None
 
 
 async def main():
-    s = Service()
-    await s.initialize()
+    # Tworzymy instancję WeatherService
+    weather_service = WeatherService()
 
+    # Tworzymy instancję StreetMonitoringService, używając create_city() do utworzenia miasta
+    street_monitoring_service = StreetMonitoringService(create_city(), weather_service)
+
+    # Uruchamiamy obie usługi w osobnych zadaniach (tasks)
+    tasks = [asyncio.create_task(weather_service.initialize()), asyncio.create_task(street_monitoring_service.initialize())]
+
+    # Czekamy pewien czas (możesz dostosować czas oczekiwania)
+    await sleep(10)
+
+    # Zatrzymujemy zadania
+    for task in tasks:
+        task.cancel()
+
+    # Wypisujemy końcową wartość current_snow_fall_rate
+    final_rate = await weather_service.get_current_snow_fall_rate()
+    logger.info(f"Final snow fall rate: {final_rate}")
 
 if __name__ == '__main__':
     run(main())
